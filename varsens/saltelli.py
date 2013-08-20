66aede531ed2a3e83270489836f2d54d8c2d87d9
Shawn Garbett <shawn@garbett.org>
2013-08-20 17:34:57 -0500

    Added 2nd order products

--- varsens/saltelli.py
+++ varsens/saltelli.py
@@ -3,7 +3,7 @@ import numpy
 import sys
 
 class Varsens(object):
-    """The main variance sensitivity object which contains the core of the computation. It will
+    '''The main variance sensitivity object which contains the core of the computation. It will
         execute the objective function n*(2*k+2) times.
         
         Parameters
@@ -39,50 +39,58 @@ class Varsens(object):
             ... 
             >>> v = Varsens(g_objective, g_scaling, 6, 1024, verbose=False)
             >>> v.var_y
-            0.5713239774588339
+            0.57105531939783061
             >>> v.E_2
             1.0040075966813939
             >>> v.sens
-            array([ 0.56933972,  0.23787565,  0.03839997,  0.00579517,  0.00178795,
-                    0.00218105])
+            array([ 0.58509195,  0.25311583,  0.03754574,  0.00752022,  0.00176028,
+                    0.00177371])
             >>> v.sens_t
-            array([ 0.71851248,  0.37028261,  0.08488697,  0.03593117,  0.0278552 ,
-                    0.0290142 ])
-    """
+            array([  7.01277445e-01,   3.54584746e-01,   5.86321223e-02,
+                     9.64126174e-03,   6.45996005e-04,   9.78965580e-04])
+    '''
     def __init__(self, objective, scaling, k, n, verbose=True):
         self.k         = k
         self.n         = n
         self.objective = objective
         self.scaling   = scaling
         self.verbose   = verbose
+        
+        # Generate sample/re-sample space
+        (M_1, M_2) = self.generate_sample()
 
-        if verbose: print "Generating Low Discrepancy Sequence"
-
-        seq = ghalton.Halton(k*2)
-        seq.get(20*k) # Remove initial linear correlated points
-        x = numpy.array(seq.get(n))
-        M_1 = scaling(x[...,0:k    ])
-        M_2 = scaling(x[...,k:(2*k)])
-    
+        # Generate the sample/re-sample permutations
         N_j  = self.generate_N_j(M_1, M_2)                                # See Eq (11)
         N_nj = self.generate_N_j(M_2, M_1)
-    
+
+        # Execute the model to determine the objective function
         (fM_1, fM_2, fN_j, fN_nj) = self.objective_values(M_1, M_2, N_j, N_nj) 
-    
-        if verbose: print "Final sensitivity calculation"
+
+        # From the model executions, compute the variable sensitivity
         self.compute_varsens(fM_1, fM_2, fN_j, fN_nj)
 
+    def generate_sample(self):
+        if self.verbose: print "Generating Low Discrepancy Sequence"
+        
+        seq = ghalton.Halton(self.k*2)
+        seq.get(20*self.k) # Remove initial linear correlated points
+        x = numpy.array(seq.get(self.n))
+        M_1 = self.scaling(x[...,     0:self.k    ])
+        M_2 = self.scaling(x[...,self.k:(2*self.k)])
+        
+        return M_1, M_2
+
     def move_spinner(self,i):
-        """A function to create a text spinner during long computations"""
+        '''A function to create a text spinner during long computations'''
         spin = ("|", "/","-", "\\")
         print " [%s] %d\r"%(spin[i%4],i),
         sys.stdout.flush()
 
     def generate_N_j(self, M_1, M_2):
-        """when passing the quasi-random low discrepancy-treated A and B matrixes, this function
+        '''when passing the quasi-random low discrepancy-treated A and B matrixes, this function
         iterates over all the possibilities and returns the C matrix for simulations.
         See e.g. Saltelli, Ratto, Andres, Campolongo, Cariboni, Gatelli, Saisana,
-        Tarantola Global Sensitivity Analysis"""
+        Tarantola Global Sensitivity Analysis'''
 
         # allocate the space for the C matrix
         N_j = numpy.array([M_2]*self.k) 
@@ -140,18 +148,31 @@ class Varsens(object):
                 fN_nj[i][j] = self.objective(N_nj[i][j])
                 if self.verbose: self.move_spinner(j)
 
+        self.fM_1 = fM_1
+        self.fM_2 = fM_2
+        self.fN_j = fN_j
+        self.fN_nj = fN_nj
+
         return fM_1, fM_2, fN_j, fN_nj
 
     def compute_varsens(self, fM_1, fM_2, fN_j, fN_nj):
+        ''' Main computation of sensitivity via Saltelli method.
+        '''
+        if self.verbose: print "Final sensitivity calculation"
+
         self.E_2 = sum(fM_1*fM_2) / self.n      # Eq (21)
 
-        # Estimate U_j and U_-j values and store them 
-        self.U_j  = numpy.sum(fM_1 * fN_j,  axis=1) / (self.n - 1)  # Eq (12)
-        self.U_nj = numpy.sum(fM_1 * fN_nj, axis=1) / (self.n - 1)  # Eq (unnumbered one after 18)
-
         #estimate V(y) from fM_1 and fM_2, paper uses only fM_1, this is a better estimate
-        self.var_y = (numpy.var(fM_1, axis=0, ddof=1)+numpy.var(fM_2, axis=0, ddof=1))/2.0
+        self.var_y = numpy.var(numpy.concatenate((fM_1, fM_2), axis=0), axis=0, ddof=1)
 
+        # Estimate U_j and U_-j values and store them, but by double method
+        self.U_j  =  numpy.sum(fM_1 * fN_j,  axis=1) / (self.n - 1)  # Eq (12)
+        self.U_j  += numpy.sum(fM_2 * fN_nj, axis=1) / (self.n - 1) 
+        self.U_j  /= 2.0
+        self.U_nj =  numpy.sum(fM_1 * fN_nj, axis=1) / (self.n - 1)  # Eq (unnumbered one after 18)
+        self.U_nj += numpy.sum(fM_2 * fN_j,  axis=1) / (self.n - 1) 
+        self.U_nj /= 2.0
+        
         #allocate the S_i and ST_i arrays
         if len(self.U_j.shape) == 1:
             self.sens   = numpy.zeros(self.k)
@@ -159,10 +180,22 @@ class Varsens(object):
         else:
             self.sens   = numpy.zeros([self.k]+[self.U_j.shape[1]])
             self.sens_t = numpy.zeros([self.k]+[self.U_j.shape[1]])
-        
 
         # now get the S_i and ST_i, Eq (27) & Eq (28)
         for j in range(self.k):
-            self.sens[j]   =       ((self.U_j[j] - self.E_2) / self.var_y)
+            self.sens[j]   = (self.U_j[j] - self.E_2) / self.var_y
             self.sens_t[j] = 1.0 - ((self.U_nj[j]- self.E_2) / self.var_y)
+
+        # Compute 2nd order terms (from double estimates)
+        self.sens_2  =  numpy.tensordot(fN_nj, fN_j,  axes=([1],[1]))
+        self.sens_2  += numpy.tensordot(fN_j,  fN_nj, axes=([1],[1]))
+        self.sens_2  /= 2.0*(self.n-1)
+        self.sens_2  -= self.E_2
+        self.sens_2  /= self.var_y
+        
+        self.sens_2n =  numpy.tensordot(fN_nj, fN_nj, axes=([1],[1]))
+        self.sens_2n += numpy.tensordot(fN_j,  fN_j,  axes=([1],[1]))
+        self.sens_2n /= 2.0 * (self.n-1)
+        self.sens_2n -= self.E_2
+        self.sens_2n /= self.var_y
 
--- varsens/tests/test_g_function.py
+++ varsens/tests/test_g_function.py
@@ -19,6 +19,23 @@ model = [0, 0.5, 3, 9, 99, 99]
 
 def g_objective(x): return g_function(x, model)
 
+def g_truth(model):
+    return 1.0/(3.0*((numpy.array(model) + 1.0)**2.0))
+
+def g_truth2(model, i, j):
+    x = g_truth(model)
+    return x[i]+x[j]+x[i]*x[j]
+
+def g_truth_t(model, i):
+    x = g_truth(model)
+
+
+#def v_i2s(model, i2s):
+#    def v_i(i):
+#        return (v_truth(model))[i]
+#    f = numpy.vectorize(v_i)
+#    return numpy.prod(f(i2s))
+
 # Would be nice to have a legendre polynomial test as well
 # http://www.jstor.org/stable/pdfplus/2676831.pdf
 #from numpy.polynomial.legendre import legval
@@ -26,11 +43,14 @@ def g_objective(x): return g_function(x, model)
 def test_g_function():
     # Analytical answer, Eq (34) divided by V(y), matches figure
     v = Varsens(g_objective, g_scaling, 6, 1024*10, verbose=False)
-    estimate = v.sens * v.var_y
-    truth    = 1.0/(3.0*((numpy.array(model) + 1.0)**2.0))
-
+    estimate  = v.sens   * v.var_y
+    estimate2 = v.sens_2 * v.var_y
+    truth    = g_truth(model)
+    
     for i in range(v.k):
         assert_almost_equal(truth[i], estimate[i], places=2)
+        for j in range(i+1, v.k):
+            assert_almost_equal(g_truth2(model, i, j), estimate2[i,j], places=2)
 
 def g_double_objective(x): return [g_function(x, model), g_function(x, model[::-1])]
 
@@ -39,7 +59,7 @@ def test_double_g_function():
     v = Varsens(g_double_objective, g_scaling, 6, 1024*10, verbose=False)
     estimate = v.sens * v.var_y
     print estimate
-    truth    = 1.0/(3.0*((numpy.array(model) + 1.0)**2.0))
+    truth    = g_truth(model)
 
     for i in range(v.k):
         assert_almost_equal(truth[i], estimate[i][0],   places=2)
